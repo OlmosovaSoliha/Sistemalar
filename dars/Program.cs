857b96c5af71eda1d6933a9ea9422667d3f02a01
OlmosovaSoliha <olmosovasoliha777@gmail.com>
2024-12-16 13:40:22 +0500

    1 versiya 10 likdan 2 likka o'tish

--- dars/Program.cs
+++ dars/Program.cs
@@ -10,6 +10,22 @@ namespace dars
     {
         static void Main(string[] args)
         {
+
+            Console.Write("10-lik sanoq sistemasidagi sonni kiriting: ");
+            int s = int.Parse(Console.ReadLine());
+
+            string natija = "";
+
+            while (s > 0)
+            {
+                int q = s % 2;
+                natija = q + natija;
+                s /= 2;
+            }
+
+            Console.WriteLine("2-lik sanoq sistemasidagi natija: " + natija);
+            Console.ReadKey();
         }
     }
+
 }
